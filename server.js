50347e6b25cd25ee09c45696ccbe602d90bc28c1
Dhavalprakashkar <62957627+Dhavalprakashkar@users.noreply.github.com>
2024-10-18 18:15:48 +0530

    update

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 import express from "express"
-// import {Login, Register} from "./controllers/auth.controllers.js"
+
 import allRoutes from "./routes/index.js"
+import dotenv from "dotenv"
 
 const app = express()
 app.use(express.json())
+dotenv.config()
 
 app.get("/",(req,res)=>{
     res.send("Home Page");
 
 })
-// app.post("/login",Login)
-
-// app.post("/register",Register)
 
 app.use('/api/v1', allRoutes)
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Running on port 8000")
 })
